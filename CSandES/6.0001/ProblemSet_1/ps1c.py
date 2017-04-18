7ed090695514088982df0aa04f51a7c545dcc3b6
Liam Luthor <liam@luthor.me>
2017-04-17 21:59:45 -0700

    Update ps1c.py

--- CSandES/6.0001/ProblemSet_1/ps1c.py
+++ CSandES/6.0001/ProblemSet_1/ps1c.py
@@ -1,8 +1,8 @@
-def getSavings(rate, salary):
+def getSavings( rate, salary ):
     goal = 250000
     r = .04
     semi_annual_raise = .07
-    month_salary = salary / 12.0
+    month_salary = salary/12.0
     deposit = float(month_salary) * rate
     savings = 0
     for i in range(1, 36):
@@ -10,9 +10,9 @@ def getSavings(rate, salary):
         savings = savings + gain + deposit
         if i % 6 == 0:
             deposit = deposit + (semi_annual_raise * deposit)
-            return savings
-
-        ### Main ###
+    return savings
+    
+### Main ###
 
 low = 0
 high = 10000
@@ -22,15 +22,16 @@ target = 250000
 epsilon = 100
 annual_salary = float(input("Enter the starting salary: "))
 
-while abs(getSavings((ans / 1000.0), annual_salary) - target) > epsilon:
+
+while abs(float(getSavings((ans/100.0), annual_salary) - target)) > epsilon:
     steps = steps + 1
-    print steps, "low =", low, " and high =", high, " and ans =", ans/1000.00
-    if getSavings((ans / 1000.0), annual_salary) > target:
+    print steps, "low =", low, " and high =", high, " and ans =", ans
+    if getSavings((ans/100.0), annual_salary) > target:
         high = ans
     else:
         low = ans
-    
+
     ans = (low + high) / 2.0
 
-print "Best savings rate: ", ans / 1000.0
-print "Steps in bisection search: ", steps
+print "Best rate is: ", ans/100.0
+print "Number of steps: ", steps
